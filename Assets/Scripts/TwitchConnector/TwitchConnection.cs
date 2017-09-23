20b166ba14d6cb7082c346ceda6d0d7c9e9fc52e
Carl Halstead <taleof4gamers2012@gmail.com>
2017-09-23 19:02:19 +0100

    Cryptography
    Added Unity-specific cryptography method which TwitchLib relies on.

--- Assets/Scripts/TwitchConnector/TwitchConnection.cs
+++ Assets/Scripts/TwitchConnector/TwitchConnection.cs
@@ -1,4 +1,7 @@
-﻿using TwitchLib;
+﻿using System;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using TwitchLib;
 using TwitchLib.Events.Client;
 using TwitchLib.Models.Client;
 using UnityEngine;
@@ -36,4 +39,36 @@ public class TwitchConnection : MonoBehaviour
         EnsureMainThread.executeOnMainThread.Enqueue(() => { FindObjectOfType<Canvas>().gameObject.SetActive(false); });
         EnsureMainThread.executeOnMainThread.Enqueue(() => { commandController.DelayedStart(); });
     }
+
+	public bool CertificateValidationMonoFix(System.Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+	{
+		bool isOk = true;
+
+		if (sslPolicyErrors == SslPolicyErrors.None)
+		{
+			return true;
+		}
+
+		foreach (X509ChainStatus status in chain.ChainStatus)
+		{
+			if (status.Status == X509ChainStatusFlags.RevocationStatusUnknown)
+			{
+				continue;
+			}
+
+			chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+			chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+			chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 1, 0);
+			chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+
+			bool chainIsValid = chain.Build((X509Certificate2)certificate);
+
+			if (!chainIsValid)
+			{
+				isOk = false;
+			}
+		}
+
+		return isOk;
+	}
 }
