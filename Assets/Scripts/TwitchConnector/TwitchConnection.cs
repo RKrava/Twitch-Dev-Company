7aa6c0ff32ed8e2768b7ff465c28ec9e3ed72cdb
KatalystKreations <mmcmullen07@hotmail.com>
2017-09-26 13:21:39 -0600

    Fixed FileNotFoundException
    Fixed the exception caused by no JSON files existing when first launching.

--- Assets/Scripts/TwitchConnector/TwitchConnection.cs
+++ Assets/Scripts/TwitchConnector/TwitchConnection.cs
@@ -7,19 +7,16 @@ using TwitchLib.Events.Client;
 using TwitchLib.Models.Client;
 using UnityEngine;
 
-public class TwitchConnection : MonoBehaviour
-{
+public class TwitchConnection : MonoBehaviour {
     public TwitchClient client { get; private set; }
 
-    CommandController commandController;
+    private CommandController commandController;
 
-    private void Awake()
-    {
+    private void Awake() {
         commandController = FindObjectOfType<CommandController>();
     }
 
-    public void Connect()
-    {
+    public void Connect() {
         ServicePointManager.ServerCertificateValidationCallback = CertificateValidationMonoFix;
 
         ConnectionCredentials credentials = new ConnectionCredentials(Settings.username, Settings.accessToken);
@@ -35,43 +32,37 @@ public class TwitchConnection : MonoBehaviour
         Debug.Log("Connected");
     }
 
-    private void ClientOnJoinedChannel(object sender, OnJoinedChannelArgs e)
-    {
+    private void ClientOnJoinedChannel(object sender, OnJoinedChannelArgs e) {
         client.SendMessage("I have arrived!");
 
         EnsureMainThread.executeOnMainThread.Enqueue(() => { FindObjectOfType<Canvas>()?.gameObject.SetActive(false); });
         EnsureMainThread.executeOnMainThread.Enqueue(() => { commandController.DelayedStart(); });
     }
 
-	public bool CertificateValidationMonoFix(System.Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
-	{
-		bool isOk = true;
+    public bool CertificateValidationMonoFix(System.Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) {
+        bool isOk = true;
 
-		if (sslPolicyErrors == SslPolicyErrors.None)
-		{
-			return true;
-		}
+        if (sslPolicyErrors == SslPolicyErrors.None) {
+            return true;
+        }
 
-		foreach (X509ChainStatus status in chain.ChainStatus)
-		{
-			if (status.Status == X509ChainStatusFlags.RevocationStatusUnknown)
-			{
-				continue;
-			}
+        foreach (X509ChainStatus status in chain.ChainStatus) {
+            if (status.Status == X509ChainStatusFlags.RevocationStatusUnknown) {
+                continue;
+            }
 
-			chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
-			chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
-			chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 1, 0);
-			chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+            chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+            chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+            chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 1, 0);
+            chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
 
-			bool chainIsValid = chain.Build((X509Certificate2)certificate);
+            bool chainIsValid = chain.Build((X509Certificate2) certificate);
 
-			if (!chainIsValid)
-			{
-				isOk = false;
-			}
-		}
+            if (!chainIsValid) {
+                isOk = false;
+            }
+        }
 
-		return isOk;
-	}
+        return isOk;
+    }
 }
