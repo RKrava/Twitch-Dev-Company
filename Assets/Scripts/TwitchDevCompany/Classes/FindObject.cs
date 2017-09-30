382fc8f99688b5f9f6312336169b44bc4ddfeba1
CreativeFletcher <creativfletcher@gmail.com>
2017-09-30 21:30:06 +0100

    Create FindObjects class
    If we're going to use it, will need to look into script order.

---
+++ Assets/Scripts/TwitchDevCompany/Classes/FindObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class FindObject : MonoBehaviour
+{
+    public static TwitchConnection twitchConnection;
+    public static FormController formController;
+    public static CommandController commandController;
+    public static SaveLoad saveLoad;
+
+    private void Awake()
+    {
+        twitchConnection = FindObjectOfType<TwitchConnection>();
+        formController = FindObjectOfType<FormController>();
+        commandController = FindObjectOfType<CommandController>();
+        saveLoad = FindObjectOfType<SaveLoad>();
+
+        formController.DelayedAwake();
+        twitchConnection.DelayedAwake();
+    }
+}
