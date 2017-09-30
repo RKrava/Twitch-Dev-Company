c9eaee54cd6f8a8a9f3f0ce77c6eb1d83db7845e
Carl Halstead <taleof4gamers2012@gmail.com>
2017-09-30 10:37:24 +0100

    DeveloperPay
    * Create a class to control how much a developer costs.
    It is mostly a dummy for now however has a method which, passed an
    amount of time, will tell you how much this developer will cost.
    
    * Fixed the inconsistent open bracket in the DeveloperClass :p

---
+++ Assets/Scripts/TwitchDevCompany/Classes/DeveloperPay.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class DeveloperPay
+{
+	/// <summary>
+	/// Per minute
+	/// This is just a placeholder default value
+	/// </summary>
+	public int pay { get; private set; } = 4;
+
+	/// <summary>
+	/// Used to calculate how much this developer will cost for a specified period of time.
+	/// e.g. 10 minutes = (4 * 10) = 40
+	/// </summary>
+	public int PayForTime(TimeSpan time) => (pay * time.Minutes);
+
+	/// <summary>
+	/// This will be used to track projects that this developer has helped to complete.
+	/// Perhaps projects get a rating out of 10 to determine how good it was? This could then
+	/// be used to help calculate this developers correct pay.
+	/// 
+	/// Currently unused however
+	/// </summary>
+	List<ProjectClass> completedProjects = new List<ProjectClass>();
+
+	public DeveloperPay(int pay)
+	{
+		this.pay = pay;
+	}
+}
