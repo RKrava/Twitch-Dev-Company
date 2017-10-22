59ad683ce8c280dd5f7d0f4a57bc00cd35d767af
Carl Halstead <taleof4gamers2012@gmail.com>
2017-10-22 11:53:35 +0100

    Comments

--- Assets/_Scripts/ExtensionMethods/StringExtensions.cs
+++ Assets/_Scripts/ExtensionMethods/StringExtensions.cs
@@ -7,8 +7,8 @@ public static class StringExtensions
     /// Use Linq to get the first character, capitalise it, then return the new string 
     /// with the capital letter
     /// </summary>
-    /// <param name="s"></param>
-    /// <returns></returns>
+    /// <param name="s">String being extended</param>
+    /// <returns>String with the first character as a capital</returns>
     public static string CapitaliseFirstLetter(this string s)
     {
         return s.First().ToString().ToUpper() + s.Substring(1);
@@ -23,8 +23,8 @@ public static class StringExtensions
     /// Use ToTitleCase to capitalise each word in the string.
     /// (Word probably being defined as anything after a space, 'This Is A Word', 'Thiswouldprobablybeasingleword')
     /// </summary>
-    /// <param name="s"></param>
-    /// <returns></returns>
+    /// <param name="s">String being extended</param>
+    /// <returns>String where every word in the string has its first character capitalised</returns>
     public static string CapitaliseAllWords(this string s)
     {
         return textInfo.ToTitleCase(s);
