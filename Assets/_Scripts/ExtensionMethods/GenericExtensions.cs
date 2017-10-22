590cc9392810c726fcb697fb38594fd9b69a586c
Carl Halstead <taleof4gamers2012@gmail.com>
2017-10-22 11:53:44 +0100

    More comments

--- Assets/_Scripts/ExtensionMethods/GenericExtensions.cs
+++ Assets/_Scripts/ExtensionMethods/GenericExtensions.cs
@@ -4,12 +4,25 @@ using System.IO;
 
 public static class GenericExtensions
 {
+    /// <summary>
+    /// This extends EVERY type. Allowing it to be saved
+    /// </summary>
+    /// <typeparam name="T">Type being extended</typeparam>
+    /// <param name="item">Object of the specific type</param>
+    /// <param name="path">Path where you would like it to be saved</param>
     public static void Save<T>(this T item, string path)
     {
         string json = JsonConvert.SerializeObject(item, Formatting.Indented);
         File.WriteAllText(path, json);
     }
 
+    /// <summary>
+    /// Take a path to a JSON text file and load it into a type
+    /// </summary>
+    /// <typeparam name="T">Type to deserialize in to</typeparam>
+    /// <param name="item">Object being extended</param>
+    /// <param name="path">Path to the JSON</param>
+    /// <returns>Object from the deserialized JSON</returns>
     public static T Load<T>(this T item, string path)
     {
         if (File.Exists(path) == false)
