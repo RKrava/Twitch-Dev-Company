032dad10730545fe6bec0eeaa4a04fa86f44d638
Carl Halstead <taleof4gamers2012@gmail.com>
2017-10-23 11:13:04 +0100

    Updated FeatureList
    Added a button to get all features within the Features folder. We are
    developers, therefore shouldnt have to do things by hand.
    Screenshot: https://i.gyazo.com/162ccf432c068af432f3c1a1b59bca8f.png

--- Assets/ScriptableObjects/_Scripts/FeatureList.cs
+++ Assets/ScriptableObjects/_Scripts/FeatureList.cs
@@ -1,10 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Create/Database/FeatureList")]
 public class FeatureList : ScriptableObject
 {
+    [Tooltip("The path to the Features folder relative to this ScriptableObject")]
+    public string featuresFolderPath = "Features";
+
     public List<string> featureNames = new List<string>();
     public List<FeatureSO> features = new List<FeatureSO>();
+
+    /// <summary>
+    /// This method has many steps:
+    /// - Clear any existing FeatureSO's as to not have duplicates.
+    /// - Get a relative path to this asset.
+    /// - Remove the file name from the path so we are just left with the directory.
+    /// - Combine the path to this asset with that of the Features folder specified above.
+    /// - Create a full path to this folder so we can read all the files.
+    /// - Look for all files with the .asset extension (Which ScriptableObjects use)
+    /// - Get a relative file path for the ScriptableObject so I can load it with AssetDatabase.
+    /// - Add it to the list of FeatureSO's
+    /// </summary>
+    #if UNITY_EDITOR
+    [ContextMenu("Get Features")]
+    public void GetFeatures()
+    {
+        features.Clear();
+
+        string relativePath = AssetDatabase.GetAssetPath(GetInstanceID());
+        string currentFolder = Path.GetDirectoryName(relativePath);
+        string featuresFolder = Path.Combine(currentFolder, featuresFolderPath);
+        string absolutePath = Path.GetFullPath(featuresFolder);
+
+        if (Directory.Exists(absolutePath) == false)
+        {
+            Debug.LogError($"Folder at: {absolutePath} does not exist! Aborting!");
+            return;
+        }
+
+        foreach (string file in Directory.GetFiles(absolutePath, "*.asset"))
+        {
+            string relativeFilePath = Path.Combine(featuresFolder, Path.GetFileName(file));
+            FeatureSO feature = (FeatureSO)AssetDatabase.LoadAssetAtPath(relativeFilePath, typeof(FeatureSO));
+            features.Add(feature);
+        }
+    }
+    #endif
 }
