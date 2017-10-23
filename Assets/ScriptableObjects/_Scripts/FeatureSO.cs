6c410d98a5c67cd49f5fe7444baeddc7218919b6
CreativeFletcher <creativfletcher@gmail.com>
2017-10-23 23:11:43 +0100

    Add comment to reflect previous change

--- Assets/ScriptableObjects/_Scripts/FeatureSO.cs
+++ Assets/ScriptableObjects/_Scripts/FeatureSO.cs
@@ -6,6 +6,11 @@ public class FeatureSO : ScriptableObject
     public string featureName;
     public int featureCost;
 
+    /// <summary>
+    /// Set to 0 by default.
+    /// In ProjectDeveloper I check if they are equal to 0.
+    /// If they are, I assume there is no requirement and set default quality to Perfect.
+    /// </summary>
     public int featureDesign = 0;
     public bool designRequired = false;
 
