e290296a98964376bcf0d8e560627f037de1c073
Carl Halstead <taleof4gamers2012@gmail.com>
2017-10-24 10:52:15 +0100

    Updated FeatureList
    * Cleaned up the code by using EditorUtility.OpenFolderPanel so we can
    get the path to the features folder without it being hard-coded.
    Image:  https://i.imgur.com/TKN1kW1.png
    
    * Created a few very simple editor buttons to get the folder, features
    or to clear the list. Nothing major, it retains the original layout just
    with added buttons.
    Image: https://i.imgur.com/Puaclqr.png

--- Assets/ScriptableObjects/_Scripts/FeatureList.cs
+++ Assets/ScriptableObjects/_Scripts/FeatureList.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 #if UNITY_EDITOR
@@ -15,6 +16,24 @@ public class FeatureList : ScriptableObject
     public List<FeatureSO> features = new List<FeatureSO>();
 
     #if UNITY_EDITOR
+    [ContextMenu("Get Folder")]
+    public void GetFolder()
+    {
+        string folderPath = EditorUtility.OpenFolderPanel("Select the 'Features' folder!", "", "");
+
+        if (string.IsNullOrEmpty(folderPath))
+        {
+            return;
+        }
+
+        Uri fullToFolder = new Uri(folderPath);
+
+        string assetsPath = Application.dataPath;
+        Uri fullToAssets = new Uri(assetsPath);
+
+        featuresFolderPath = fullToAssets.MakeRelativeUri(fullToFolder).ToString();
+    }
+
     /// <summary>
     /// This method has many steps:
     /// - Clear any existing FeatureSO's as to not have duplicates.
@@ -26,16 +45,12 @@ public class FeatureList : ScriptableObject
     /// - Get a relative file path for the ScriptableObject so I can load it with AssetDatabase.
     /// - Add it to the list of FeatureSO's
     /// </summary>
-
     [ContextMenu("Get Features")]
     public void GetFeatures()
     {
         features.Clear();
 
-        string relativePath = AssetDatabase.GetAssetPath(GetInstanceID());
-        string currentFolder = Path.GetDirectoryName(relativePath);
-        string featuresFolder = Path.Combine(currentFolder, featuresFolderPath);
-        string absolutePath = Path.GetFullPath(featuresFolder);
+        string absolutePath = Path.GetFullPath(featuresFolderPath);
 
         if (Directory.Exists(absolutePath) == false)
         {
@@ -45,7 +60,7 @@ public class FeatureList : ScriptableObject
 
         foreach (string file in Directory.GetFiles(absolutePath, "*.asset"))
         {
-            string relativeFilePath = Path.Combine(featuresFolder, Path.GetFileName(file));
+            string relativeFilePath = Path.Combine(featuresFolderPath, Path.GetFileName(file));
             FeatureSO feature = (FeatureSO)AssetDatabase.LoadAssetAtPath(relativeFilePath, typeof(FeatureSO));
             features.Add(feature);
         }
